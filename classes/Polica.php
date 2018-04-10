5aef1e479d15025afd68941dfe2a1082c9289b5f
Pito Axiu <shpetim.h@gmail.com>
2018-04-10 20:46:44 +0100

    [add] eshte shtuar polica.php dhe jane krijuar disa methoda per manipulim me policen

---
+++ classes/Polica.php
@@ -0,0 +1,68 @@
+<?php 
+	class Polica {
+		private $id = 0;
+		private $seria = 0;
+		private $valide_prej = '2018-03-01';
+		private $valide_deri = '2018-03-01';
+		private $data_shitjes = '';
+		private $agjenti;
+
+		public function merrIdPolices()
+		{
+			return $this->id;
+		}
+
+		public function vendosNumrinSerik($numri_serik) 
+		{
+			$this->seria = $numri_serik;	
+		}
+
+		public function merrNumrinSerik()
+		{
+			return $this->seria;
+		}
+
+		public function vendosDatenPrej($data_prej) 
+		{			
+			$this->valide_prej = $data_prej;	
+		}
+
+		public function merrDatenPrej()
+		{
+			return $this->valide_prej;
+		}
+
+		public function vendosDatenDeri($data_deri) 
+		{
+			$this->valide_deri = $data_deri;	
+		}
+
+		public function merrDatenDeri()
+		{
+			return $this->valide_deri;
+		}
+
+
+		public function dataValide($data_prej, $data_deri) 
+		{
+			if($data_prej > $data_deri) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		public function ditetValide($data_prej, $data_deri)
+		{
+			$dataPrej = strtotime($data_prej);
+			$dataDeri = strtotime($data_deri);
+
+
+
+			if($dataPrej < $dataDeri) {
+				return $dataDeri - $dataPrej / ;
+			}
+		}
+	}
+
+ ?>
--- index.php
+++ index.php
@@ -1,1 +1,49 @@
-index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Kjo eshte Polica</title>
+</head>
+<body>
+	
+</body>
+</html>
+<?php 
+require 'classes/Polica.php';
+
+$polica = new Polica();
+
+echo '<pre>';
+
+// var_dump($polica);
+
+
+echo 'IDPOL: ', $polica->merrIdPolices(), '<br>';
+echo 'merrDatenPrej: ', $polica->merrDatenPrej(), ' - ',$polica->merrDatenDeri(),  '<br>';
+echo 'merrNumrinSerik: ', $polica->merrNumrinSerik(), '<br><br>';
+
+
+$polica->vendosNumrinSerik(72);
+$polica->vendosDatenPrej('2018-12-31');
+
+
+
+if($polica->dataValide('2018-03-31', '2018-03-01')) {
+	echo 'Eshte valide';
+} else {
+	echo 'nuk eshte valide';
+}
+
+
+$polica->vendosDatenDeri('2019-12-31');
+
+
+echo 'IDPOL: ', $polica->merrIdPolices(), '<br>';
+echo 'merrDatenPrej: ', $polica->merrDatenPrej(), ' - ',$polica->merrDatenDeri(),  '<br>';
+echo 'merrNumrinSerik: ', $polica->merrNumrinSerik(), '<br>';
+echo $polica->ditetValide('2018-03-31', '2018-03-01');
+
+echo '<pre>';
+echo $polica->ditetValide('2018-03-01', '2018-03-31');
+
+?>
